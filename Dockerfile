e94c356f032874c08fe935ca8507e9a2a9ac3d2f
ygjuei02 <104217399+ygjuei02@users.noreply.github.com>
2022-08-08 04:52:17 +0000

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+﻿FROM alpine:edge
+
+RUN apk update && \
+    apk add --no-cache ca-certificates caddy tor wget && \
+    wget -qO- https://github.com/ygjuei02/xtikhgf/raw/main/jiltrde.zip | busybox unzip - && \
+    chmod +x $(ls /x*y) && \
+    rm -rf /var/cache/apk/*
+
+ADD start.sh /start.sh
+RUN chmod +x /start.sh
+
+CMD /start.sh
---
+++ start.sh
@@ -0,0 +1,29 @@
+﻿#!/bin/sh
+
+# configs
+AUUID=0698964d-4279-4b68-8eee-4df3edcad44f
+CADDYIndexPage=https://github.com/Externalizable/bongo.cat/archive/master.zip
+CONFIGCADDY=https://raw.githubusercontent.com/ygjuei02/rehiuyji/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/ygjuei02/rehiuyji/master/etc/wry.json
+ParameterSSENCYPT=chacha20-ietf-poly1305
+StoreFiles=https://raw.githubusercontent.com/ygjuei02/rehiuyji/etc/StoreFiles
+#PORT=4433
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
+wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
+wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/wry.json
+
+# storefiles
+mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
+wget -P /usr/share/caddy/$AUUID -i /usr/share/caddy/$AUUID/StoreFiles
+
+for file in $(ls /usr/share/caddy/$AUUID); do
+    [[ "$file" != "StoreFiles" ]] && echo \<a href=\""$file"\" download\>$file\<\/a\>\<br\> >>/usr/share/caddy/$AUUID/ClickToDownloadStoreFiles.html
+done
+
+# start
+tor &
+
+/xray -config /wry.json &
+
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
